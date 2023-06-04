059a9aab6bc447c2e5589f58619c77d35ec315cc
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-06-04 14:41:04 +0300

    object

---
+++ js/object/object.js
@@ -0,0 +1,20 @@
+// Создать объект с полем product, равным ‘iphone’
+
+const goods = {
+    product: "iphone"
+}
+
+// Добавить в объект поле price, равное 1000 и поле currency, равное ‘dollar’
+goods.price = 1000;
+goods.currency = 'dollar'
+
+// item['price'] = 1000;
+// item['currency'] = "dollar";
+
+
+// Добавить поле details, которое будет содержать объект с полями model и color
+goods.details = {}
+
+goods.details.model =''
+goods.details.color =''
+console.log(goods)
