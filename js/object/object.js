82792244c2248f6965cbef7c4495963c8489e61b
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-06-04 17:29:15 +0300

    statement

--- js/object/object.js
+++ js/object/object.js
@@ -19,26 +19,4 @@ goods.details.model =''
 goods.details.color =''
 console.log(goods)
 
-// Чему равно а, почему?
 
-// let a = 0 || 'string'; //  'string'
-// let a = 1 && 'string'; // 'string'
-// let a = null || 25; // 25
-// let a = null && 25; //null
-// let a = null || 0 || 35; //35
-let a = null && 0 && 35;  //null
-
-
-
-// Что отобразится в консоли. Почему?
-
-12 + 14 + '12' //2612
-3 + 2 - '1' //4
-'3' + 2 - 1//31
-true + 2 //3
-+'10' + 1 //11
-undefined + 2 //Nan
-null + 5//5
-true + undefined //Nan
-
-console.log(true + undefined)
