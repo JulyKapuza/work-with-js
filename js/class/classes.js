6a0e4e90ebecdde4b46d89c8a220f25b6499408b
Juliia Kapuza <kapuzajuliia@gmail.com>
2023-07-23 19:17:15 +0300

    classes

--- js/class/classes.js
+++ js/class/classes.js
@@ -6,3 +6,18 @@ const tools = {
 tools.age; // "Age is 15"
 tools.age = 25;
 tools.age; // "Age is 25"
+
+class User {
+  constructor(userName) { 
+    this.un = userName; 
+  }
+
+  get name() { return 'User name is ' + this.un; }
+  set name(value) {this.un = value; }
+}
+const u1 = new User('Luter');
+
+
+u1.name;  // "User name is Luter"
+u1.name = 'Kolya';
+u1.name; // "User name is Kolya"
